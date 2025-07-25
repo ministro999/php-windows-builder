ac7f576ac41991403be7b6ccd3c071f94b98bb2d
ministro999 <misterionso@gmail.com>
2025-07-25 04:53:22 +0300

    Update Add-Extension.ps1

--- extension/BuildPhpExtension/private/Add-Extension.ps1
+++ extension/BuildPhpExtension/private/Add-Extension.ps1
@@ -23,29 +23,38 @@ Function Add-Extension {
     process {
         Set-GAGroup start
         Invoke-WebRequest -Uri "https://pecl.php.net/get/$Extension" -OutFile "$Extension.tgz"
+
+        $cur_Extension = $Extension       
+        if ($Extension -match '^(.*?)-') {
+            $cur_Extension = $matches[1]
+        }        
         $currentDirectory = (Get-Location).Path
-        & tar -xzf "$Extension.tgz" -C $currentDirectory
-        Set-Location "$Extension-*"
+        if (-not (Test-Path "$currentDirectory\$cur_Extension")) {
+            New-Item -Path "$currentDirectory\$cur_Extension" -ItemType Directory
+        }
+        & tar -xzf "$Extension.tgz" -C  "$currentDirectory\$cur_Extension"
+        Move-Item -Path "$currentDirectory\$cur_Extension\$Extension\*" -Destination "$currentDirectory\$cur_Extension" -Force        
+        Set-Location "$cur_Extension"
         $extensionBuildDirectory = Join-Path -Path (Get-Location).Path -ChildPath $config.build_directory
         # Apply patches only for php/php-windows-builder and shivammathur/php-windows-builder
         if($null -ne $env:GITHUB_REPOSITORY) {
             if($env:GITHUB_REPOSITORY -eq 'php/php-windows-builder' -or $env:GITHUB_REPOSITORY -eq 'shivammathur/php-windows-builder') {
-                if(Test-Path -PATH $PSScriptRoot\..\patches\$Extension.ps1) {
-                    . $PSScriptRoot\..\patches\$Extension.ps1
+                if(Test-Path -PATH $PSScriptRoot\..\patches\$cur_Extension.ps1) {
+                    . $PSScriptRoot\..\patches\$cur_Extension.ps1
                 }
             }
         }
         $configW32Content = [string](Get-Content -Path "config.w32")
-        $argument = Get-ArgumentFromConfig $Extension $configW32Content
+        $argument = Get-ArgumentFromConfig $cur_Extension $configW32Content
         $bat_content = @()
         $bat_content += ""
         $bat_content += "call phpize 2>&1"
         $bat_content += "call configure --with-php-build=`"..\deps`" $argument --with-mp=`"disable`" --with-prefix=$Prefix 2>&1"
         $bat_content += "nmake /nologo 2>&1"
         $bat_content += "exit %errorlevel%"
-        Set-Content -Encoding "ASCII" -Path $Extension-task.bat -Value $bat_content
+        Set-Content -Encoding "ASCII" -Path $cur_Extension-task.bat -Value $bat_content
         $builder = "$currentDirectory\php-sdk\phpsdk-starter.bat"
-        $task = (Get-Item -Path "." -Verbose).FullName + "\$Extension-task.bat"
+        $task = (Get-Item -Path "." -Verbose).FullName + "\$cur_Extension-task.bat"
         $suffix = "php_" + (@(
             $Config.name,
             $Config.ref,
@@ -57,11 +66,11 @@ Function Add-Extension {
         & $builder -c $Config.vs_version -a $Config.Arch -s $Config.vs_toolset -t $task | Tee-Object -FilePath "build-$suffix.txt"
         Write-Host (Get-Content "build-$suffix.txt" -Raw)
         $includePath = "$currentDirectory\php-dev\include"
-        New-Item -Path $includePath\ext -Name $Extension -ItemType "directory" | Out-Null
-        Get-ChildItem -Path (Get-Location).Path -Recurse -Include '*.h', '*.c' | Copy-Item -Destination "$includePath\ext\$Extension"
+        New-Item -Path $includePath\ext -Name $cur_Extension -ItemType "directory" | Out-Null
+        Get-ChildItem -Path (Get-Location).Path -Recurse -Include '*.h', '*.c' | Copy-Item -Destination "$includePath\ext\$cur_Extension"
         Copy-Item -Path "$extensionBuildDirectory\*.dll" -Destination "$currentDirectory\php-bin\ext" -Force
         Copy-Item -Path "$extensionBuildDirectory\*.lib" -Destination "$currentDirectory\php-dev\lib" -Force
-        Add-Content -Path "$currentDirectory\php-bin\php.ini" -Value "extension=$Extension"
+        Add-Content -Path "$currentDirectory\php-bin\php.ini" -Value "extension=$cur_Extension"
         Set-Location $currentDirectory
         Set-GAGroup end
     }
